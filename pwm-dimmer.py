6563dd3309a9372950d24a2f301dc7b5ca74f018
Carsten Koester <carsten@ckoester.net>
2019-01-17 09:24:46 +0000

    Add quick-and-dirty PWM dimmer

---
+++ pwm-dimmer.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+"""
+OLA to Raspberry Pi GPIO ports PWM dimmer.
+
+Consumes five DMX channels,
+
+  Intensity. Red, Blue. Green. White
+
+and provides PWM-dimmed output for a RGBW LED
+strip.
+"""
+import RPi.GPIO as IO
+from ola.ClientWrapper import ClientWrapper
+
+# GPIO pins associated with R, G, B and W channel in that order
+GPIO_PORTS = [12, 13, 18, 19]
+# DMX start address
+START = 100
+# OLA Universe
+UNIVERSE = 1
+
+IO.setwarnings(False)
+IO.setmode (IO.BCM)
+
+# Callback for new data
+def NewData(data):
+    intens = data[START-1]
+    for (i, p) in enumerate(GPIO_PORTS):
+        print("Index {} addr {} gpio {} rvalue {} intens {} value {} cycle {}".format(
+            i, START+i, p, data[START+i], intens, intens*data[START+i]/255, intens*data[START+i]*100/255/255))
+        pwm[p].ChangeDutyCycle(intens * data[START+i] * 100 / 255 / 255)
+
+# Setup
+def init():
+    pwm = {}
+    for p in GPIO_PORTS:
+        IO.setup(p, IO.OUT)
+        pwm[p] = IO.PWM(p, 100)
+        pwm[p].start(0)
+
+init()
+wrapper = ClientWrapper()
+client = wrapper.Client()
+client.RegisterUniverse(UNIVERSE, client.REGISTER, NewData)
+wrapper.Run()
